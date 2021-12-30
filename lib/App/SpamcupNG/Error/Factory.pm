f0c7c8971be36acf38b1a05c7cd5f71e3cc37e83
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2021-12-30 17:52:52 -0300

    doc: added missing Pod

--- lib/App/SpamcupNG/Error/Factory.pm
+++ lib/App/SpamcupNG/Error/Factory.pm
@@ -9,11 +9,47 @@ use App::SpamcupNG::Error::Bounce;
 
 # VERSION
 
+=head1 NAME
+
+App::SpamcupNG::Error::Factory - factory design pattern to create new instances
+of errors parsed from Spamcop website HTML.
+
+=head1 SYNOPSIS
+
+    use App::SpamcupNG::Error::Factory qw(create_error);
+
+=head1 DESCRIPTION
+
+=head1 EXPORTS
+
+The function C<create_error> is the only things exported by this module.
+
+=cut
+
 our @EXPORT_OK = qw(create_error);
 
 my $mailhost_regex = qr/Mailhost\sconfiguration\sproblem/;
 my $bounce_regex   = qr/bounce/;
 my @fatal_errors   = ( qr/email\sis\stoo\sold/, qr/^Nothing/ );
+
+=head1 FUNCTIONS
+
+=head2 create_error
+
+Creates new error from a given message string.
+
+The type of error is identified from this message.
+
+Expects as parameters:
+
+- an array reference where each index is a string line from the original error
+message.
+
+- a integer, being 0 if the error message is not fatal, 1 otherwise.
+
+Returns an instance of App::SpamcupNG::Error or one of it's subclasses.
+
+=cut
 
 sub create_error {
     my ( $message_ref, $is_fatal ) = @_;
@@ -41,3 +77,31 @@ sub create_error {
 
     return App::SpamcupNG::Error->new( $message_ref, $is_fatal );
 }
+
+=head1 AUTHOR
+
+Alceu Rodrigues de Freitas Junior, E<lt>arfreitas@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2018 of Alceu Rodrigues de Freitas Junior,
+E<lt>arfreitas@cpan.orgE<gt>
+
+This file is part of App-SpamcupNG distribution.
+
+App-SpamcupNG is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+App-SpamcupNG is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+App-SpamcupNG. If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
+1;
+
