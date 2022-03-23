0b0c70ead698b143a9dadf6d55350cc8312018c3
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2022-03-22 22:30:17 -0300

    feat: added unix epoch when summary was stored

--- lib/App/SpamcupNG/Summary/Recorder.pm
+++ lib/App/SpamcupNG/Summary/Recorder.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use Carp qw(confess);
 use DBI 1.643;
+use DateTime 1.55;
 
 # VERSION
 
@@ -14,6 +15,14 @@ App::SpamcupNG::Summary::Recorder - class to save Summary to SQLite3
 
 =head1 SYNOPSIS
 
+    use App::SpamcupNG::Summary::Recorder;
+
+    # just pretend that $summary is an existing App::SpamcupNG::Summary instance
+    my $recorder = App::SpamcupNG::Summary::Recorder->new(
+        '/some/path/database_file' );
+    $recorder->init;
+    $recorder->save($summary);
+
 =head1 DESCRIPTION
 
 This class is used to persist L<App::SpamcupNG::Summary> instances to a SQLite3
@@ -22,6 +31,11 @@ database.
 =head1 METHODS
 
 =head2 new
+
+Creates a new recorder instance.
+
+Expects as parameter the complete path to a existing (or to create) SQLite 3
+file.
 
 =cut
 
@@ -38,7 +52,7 @@ sub new {
 
 =head2 init
 
-Initialize the database if it doesn't exist yet.
+Initialize the database if it doesn't exist yet. This is idempotent.
 
 =cut
 
@@ -58,7 +72,7 @@ CREATE TABLE IF NOT EXISTS email_content_type (
 CREATE TABLE IF NOT EXISTS spam_age_unit (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL UNIQUE
-)    
+)
     }
     ) or die $self->{dbh}->errstr;
 
@@ -94,6 +108,7 @@ CREATE TABLE IF NOT EXISTS mailer (
 CREATE TABLE IF NOT EXISTS summary (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   tracking_id TEXT NOT NULL UNIQUE,
+  created INTEGER NOT NULL,
   charset_id INTEGER REFERENCES email_charset ON DELETE SET NULL,
   content_type_id INTEGER REFERENCES email_content_type ON DELETE SET NULL,
   age INTEGER NOT NULL,
@@ -118,7 +133,7 @@ CREATE TABLE IF NOT EXISTS summary_receiver (
 
 =head2 save
 
-Persists a L<App::Spamcup::Summary> instance to the database.
+Persists a L<App::SpamcupNG::Summary> instance to the database.
 
 =cut
 
@@ -162,15 +177,17 @@ VALUES(?, ?, ?)
 
 sub _save_summary {
     my ( $self, $summary, $fields_ref ) = @_;
+    my $now    = DateTime->now;
     my $insert = q{
 INSERT INTO summary
-(tracking_id, charset_id, content_type_id, age, age_unit_id, mailer_id)
-VALUES (?, ?, ?, ?, ?, ?)    
+(tracking_id, created, charset_id, content_type_id, age, age_unit_id, mailer_id)
+VALUES (?, ?, ?, ?, ?, ?, ?)
 };
     my @values = (
-        $summary->get_tracking_id,   $fields_ref->{charset_id},
-        $fields_ref->{content_type}, $summary->get_age,
-        $fields_ref->{age_unit},     $fields_ref->{mailer}
+        $summary->get_tracking_id, $now->epoch,
+        $fields_ref->{charset_id}, $fields_ref->{content_type},
+        $summary->get_age,         $fields_ref->{age_unit},
+        $fields_ref->{mailer}
     );
     $self->{dbh}->do( $insert, undef, @values )
         or confess $self->{dbh}->errstr;
@@ -217,7 +234,41 @@ INSERT INTO $table ($column) VALUES (?)
     return $self->{dbh}->last_insert_id;
 }
 
+=head2 DESTROY
+
+Properly closes the SQLite 3 database file when the recorder instance goes out
+of scope.
+
+=cut
+
+sub DESTROY {
+    my $self = shift;
+
+    if ( $self->{dbh} ) {
+        $self->{dbh}->disconnect or warn $self->{dbh}->errstr;
+    }
+}
+
 =pod
+
+=head1 QUERYING RESULTS
+
+This is a sample query to checkout records in the database:
+
+    SELECT A.id,
+      A.tracking_id,
+      DATETIME(A.created, 'unixepoch') AS CREATED,
+      B.name AS CHARSET,
+      C.name AS CONTENT_TYPE,
+      A.age,
+      D.name AS MAILER,
+      E.report_id,
+      F.email
+    FROM summary A outer left join email_charset B on A.charset_id = B.id
+    INNER JOIN email_content_type C ON A.content_type_id = C.id
+    OUTER LEFT JOIN mailer D ON A.mailer_id = D.id
+    INNER JOIN summary_receiver E ON A.id = E.summary_id
+    INNER JOIN receiver F ON E.receiver_id = F.id;
 
 =head1 SEE ALSO
 
@@ -232,4 +283,3 @@ L<https://www.sqlite.org/docs.html>
 =cut
 
 1;
-
